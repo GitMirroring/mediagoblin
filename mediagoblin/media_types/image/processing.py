f943ebc11cc4e296d0b283add9228c2376f91846
Ben Sturmfels <ben@sturm.com.au>
2023-08-14 17:32:45 +1000

    Update Pillow filters to fix DeprecationWarning
    Warning with Pillow 9.2.0, Python 3.10 and `PYTHONDEVMODE=1` was:
    
    mediagoblin/media_types/image/processing.py:40: DeprecationWarning: NEAREST is deprecated and will be removed in Pillow 10 (2023-07-01). Use Resampling.NEAREST or Dither.NONE instead.
      'NEAREST': Image.NEAREST,
    mediagoblin/media_types/image/processing.py:41: DeprecationWarning: BILINEAR is deprecated and will be removed in Pillow 10 (2023-07-01). Use Resampling.BILINEAR instead.
      'BILINEAR': Image.BILINEAR,
    mediagoblin/media_types/image/processing.py:42: DeprecationWarning: BICUBIC is deprecated and will be removed in Pillow 10 (2023-07-01). Use Resampling.BICUBIC instead.
      'BICUBIC': Image.BICUBIC,
    mediagoblin/media_types/image/processing.py:43: DeprecationWarning: ANTIALIAS is deprecated and will be removed in Pillow 10 (2023-07-01). Use Resampling.LANCZOS instead.
      'ANTIALIAS': Image.ANTIALIAS}

--- mediagoblin/media_types/image/processing.py
+++ mediagoblin/media_types/image/processing.py
@@ -37,10 +37,10 @@ from mediagoblin.tools.exif import exif_fix_image_orientation, \
 _log = logging.getLogger(__name__)
 
 PIL_FILTERS = {
-    'NEAREST': Image.NEAREST,
-    'BILINEAR': Image.BILINEAR,
-    'BICUBIC': Image.BICUBIC,
-    'ANTIALIAS': Image.ANTIALIAS}
+    'NEAREST': Image.Resampling.NEAREST,
+    'BILINEAR': Image.Resampling.BILINEAR,
+    'BICUBIC': Image.Resampling.BICUBIC,
+    'ANTIALIAS': Image.Resampling.LANCZOS}
 
 MEDIA_TYPE = 'mediagoblin.media_types.image'
 
