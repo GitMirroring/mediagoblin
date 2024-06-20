d4b2f5b67c6862346e0f91b5e964d5b07878046d
Ben Sturmfels <ben@sturm.com.au>
2024-06-20 14:08:14 +1000

    Add Guix channel

---
+++ .guix/modules/mediagoblin-package.scm
@@ -0,0 +1,127 @@
+;;; GNU MediaGoblin -- federated, autonomous media hosting
+;;; Copyright 2015, 2016 David Thompson <davet@gnu.org>
+;;; Copyright 2016 Christopher Allan Webber <cwebber@dustycloud.org>
+;;; Copyright 2019, 2020, 2021, 2024 Ben Sturmfels <ben@sturm.com.au>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+
+(define-module (mediagoblin)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system python)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages openldap)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages python-crypto)
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages sphinx)
+  #:use-module (gnu packages gstreamer)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages time)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages xml))
+
+;; See README.md for usage instructions and caveats.
+
+(define-public mediagoblin
+  (let ((commit "4258a7392791462c9589fb8fbc347a26345498ea")
+        (revision "1"))
+    (package
+      (name "mediagoblin")
+      (version (git-version "0.14.0.dev" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://git.sr.ht/~mediagoblin/mediagoblin")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "13h4947c37fafmlsg9qq02if10qs2bbp9sm7lw4v9l4n0x5rq1cj"))))
+      (build-system python-build-system)
+      (arguments
+       `(#:phases (modify-phases %standard-phases
+                    ;; Build the language translations
+                    (add-after 'build 'build-translations
+                      (lambda _
+                        (invoke "devtools/compile_translations.sh")))
+                    ;; We're using the pytest test runner
+                    (replace 'check
+                      (lambda* (#:key tests? #:allow-other-keys)
+                        (when tests?
+                          (invoke "pytest" "mediagoblin/tests" "-rs"
+                                  "--forked"))))
+                    ;; Avoid system-error "utime" "~A" ("No such file or directory")
+                    ;; TODO: Is there a better way to fix this?
+                    (delete 'ensure-no-mtimes-pre-1980))))
+      (native-inputs (list gobject-introspection
+                           python-pytest
+                           python-pytest-forked
+                           python-pytest-xdist
+                           python-sphinx
+                           python-webtest))
+      (inputs (list python-alembic
+                    python-babel
+                    python-celery
+                    python-configobj
+                    python-dateutil
+                    python-exif-read
+                    python-feedgenerator
+                    python-itsdangerous
+                    python-jinja2
+                    python-jsonschema
+                    python-ldap ;For LDAP plugin
+                    python-lxml
+                    python-markdown
+                    python-oauthlib
+                    python-openid ;For OpenID plugin
+                    python-pastescript
+                    python-pillow
+                    python-bcrypt
+                    python-pyld
+                    python-pytz
+                    python-redis ;Simplest Celery backend
+                    python-requests ;For batchaddmedia
+                    python-soundfile
+                    python-sqlalchemy
+                    python-unidecode
+                    python-waitress
+                    python-werkzeug
+                    python-wtforms
+                    python-wtforms-sqlalchemy
+
+                    ;; Audio/video media
+                    gst-libav
+                    gst-plugins-bad
+                    gst-plugins-base
+                    gst-plugins-good
+                    gst-plugins-ugly
+                    gstreamer
+                    openh264
+                    python-gst ;For tests to pass
+                    python-numpy ;Audio spectrograms
+                    python-pygobject
+
+                    ;; PDF media.
+                    poppler))
+      (home-page "https://mediagoblin.org/")
+      (synopsis "Web application for media publishing")
+      (description
+       "MediaGoblin is a free software media publishing platform that anyone can
+run.  You can think of it as a decentralized alternative to Flickr, YouTube,
+SoundCloud, etc.")
+      (license (list license:agpl3+ license:cc0)))))
+
+mediagoblin
